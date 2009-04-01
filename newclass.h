f02aea369dae22b63546c5ff9849f1a6a96e40fa
riku87 <riku87@7c34b0ef-0d9b-4d33-a6fb-01a43cae59b2>
2009-04-01 10:11:50 +0000

    prueba commit en netbeans

---
+++ newclass.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   newclass.h
+ * Author: riku
+ *
+ * Created on 1 de abril de 2009, 12:09
+ */
+
+#ifndef _NEWCLASS_H
+#define	_NEWCLASS_H
+
+class NewClass {
+public:
+    NewClass();
+    NewClass(const NewClass& orig);
+    virtual ~NewClass();
+private:
+
+};
+
+#endif	/* _NEWCLASS_H */
+
