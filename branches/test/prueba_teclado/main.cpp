60c023fca01335ee5d008b3113cc4755dc53ea45
rosunix <rosunix@7c34b0ef-0d9b-4d33-a6fb-01a43cae59b2>
2009-04-02 19:31:39 +0000

    [Branches/test] animación con teclado modificado, el sprite no sale de la pantalla.

--- branches/test/prueba_teclado/main.cpp
+++ branches/test/prueba_teclado/main.cpp
@@ -54,10 +54,10 @@ int main() {
 
     // Creamos animaciones para llenar la pantalla
 
-    Animacion animacion("./dentacos.png", 4, 4,"4,5,6,7,6,5,4", 120);  
-    Animacion animacion1("./dentacos.png", 4, 4,"12,13,14,15,14,13,12", 120);  
-    Animacion animacion2("./dentacos.png", 4, 4,"8,9,10,11,10,9,8", 120);  
-    Animacion animacion3("./dentacos.png", 4, 4,"0,1,2,3,2,1,0", 120); 
+    Animacion animacion("./dentacos.png", 4, 4,"4,5,6,7", 120);  
+    Animacion animacion1("./dentacos.png", 4, 4,"12,13,14,15", 120);  
+    Animacion animacion2("./dentacos.png", 4, 4,"8,9,10,11", 120);  
+    Animacion animacion3("./dentacos.png", 4, 4,"0,1,2,3", 120); 
     
     // animacion.animar(pantalla, 100, 100);
 
@@ -89,14 +89,20 @@ int main() {
 
     
     // Bucle "infinito"
+    
+    int x = 4, y = 4;
 
-    for(int x = 0, y = 0 ; ; ) {
+    for( ; ; ) {
 
-	// Si se sale de la pantalla volvemos a introducirlo
+	// El muñeco COMPLETO no se sale de la pantalla.
 
-	if(x == TAM_X)
-	    x = 0;
-	if(y == TAM_Y)
+	if(x > 452)
+	  x = 452;
+	else if(x < 0)
+	  x = 0;
+	if(y > 308)
+	  y = 308;
+	else if(y < 0)
 	  y = 0;
 
 	// Referencia de tiempo
@@ -104,48 +110,30 @@ int main() {
 	t1 = SDL_GetTicks();
 	
 	if((t1 - t0) > animacion.retardo()) {
-	    
-	    // Nueva referencia de tiempo
-	    
-	    t0 = SDL_GetTicks();
-
-	    // Movimiento del personaje
-	    /*if(evento.type == SDL_KYDOWN && evento.)
-	      y += 4;*/
-	    // Limpiamos la pantalla
-	    // Sería mejor limpiar el anterior
-
-	    //SDL_FillRect(pantalla, NULL,				\
-	    //	 SDL_MapRGB(pantalla->format, 0, 0, 0));
-
-	    // Mostramos el siguiente paso de todas las animaciones
-	    /*
-	    if(x % 16 == 0)
-	      animacion.paso_a_paso(pantalla, x, y,-1);
-	    else{
-	      y += 4;
-	      animacion3.paso_a_paso(pantalla,x,y);
-	      }*/
-	    //animacion1.paso_a_paso(pantalla, 200, 300);
-	    //animacion2.paso_a_paso(pantalla, 300, 300);
+	  t0 = SDL_GetTicks();
 	}
 
 	
 	// Control de la entrada
 
 	while(SDL_PollEvent(&evento)) {
+	  cout << "coordenadas: (" << x << "," << y << ")." << endl;
 	    
 	    if(evento.type == SDL_KEYDOWN) {
 	      SDL_FillRect(pantalla, NULL,			\
 			   SDL_MapRGB(pantalla->format, 0, 0, 0));
 	      switch(evento.key.keysym.sym){
-	      case SDLK_DOWN: y+=4;
+	      case SDLK_DOWN: 
+		y+=4; // title abajo
 		animacion3.paso_a_paso(pantalla, x,y); break;
-	      case SDLK_UP: y-=4;
+	      case SDLK_UP: 
+		y-=4; // title arriba 
 		animacion1.paso_a_paso(pantalla, x,y); break;
-	      case SDLK_RIGHT: x+=4;
+	      case SDLK_RIGHT: 
+		x+=4; // title derecha
 		animacion2.paso_a_paso(pantalla,x,y); break;
-	      case SDLK_LEFT: x-=4;
+	      case SDLK_LEFT: 
+		x-=4; // title izquierda
 		animacion.paso_a_paso(pantalla,x,y); break;
 	      default: break;
 	      }
