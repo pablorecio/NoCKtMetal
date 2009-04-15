cbf05482a104f0527f11ddbc16eb08d3437af8ea
rosunix <rosunix@7c34b0ef-0d9b-4d33-a6fb-01a43cae59b2>
2009-04-15 18:32:43 +0000

    [trunk/src] Añadido clase tile. Falta documentación.

---
+++ trunk/src/tile.h
@@ -0,0 +1,108 @@
+
+//-*-C++-*-
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ * Copyright (C) 2009, NoCKTMetal Team
+ *
+ */
+
+/*
+ * --------------------------------------------------------
+ * Fichero = tile.h
+ * Autor = Rosa María Durante Lerate
+ * Fecha = 15 / 04 / 2009
+ */
+
+#ifndef _TILE_H_
+#define _TILE_H_
+
+#include <SDL/SDL.h>
+
+
+/** 
+ * Clase que abstrae el concepto de tile como mínima imagen que utilizaremos en este videojuego.
+ * 
+ * @author Rosa María Durante Lerate 
+ */
+
+class Tile{
+ public:
+
+
+  /** 
+   * @brief
+   * @param ruta
+   * @param col
+   * @param inter
+   */
+
+  Tile(char *ruta, bool col=false, bool inter=false);
+
+
+  /** 
+   * @brief
+   */
+
+  ~Tile();
+
+
+  /** 
+   * @brief
+   * @return
+   */
+
+  Uint32 getAnchura();
+
+
+  /** 
+   * @brief
+   * @return 
+   */
+
+  Uint32 getAltura();
+
+
+  /** 
+   * @brief
+   * @return
+   */
+
+  SDL_Surface* getImagen();
+  
+
+  /** 
+   * @brief
+   * @return
+   */
+
+  bool isColisionable();
+
+
+  /** 
+   * @brief
+   * @return 
+   */
+
+  bool isInteractuable();
+
+ private:
+  SDL_Surface *_imagen;
+  Uint32 _colorkey;
+  bool _colisionable;
+  bool _interactuable;
+
+};
+
+#endif
