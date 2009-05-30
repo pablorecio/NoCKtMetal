598bc5fd1f9136363096c4c7849d490e18ef5334
rosunix <rosunix@7c34b0ef-0d9b-4d33-a6fb-01a43cae59b2>
2009-05-30 11:30:53 +0000

    [trunk] Comenzada la clase NPJ. Añadido comentarios para saber cm estructurar la clase

--- trunk/src/npj.h
+++ trunk/src/npj.h
@@ -26,4 +26,27 @@
 #ifndef _NPJ_H_
 #define _NPJ_H_
 
+/* . Esta clase estará relacionada con la clase IMAGEN ¿Por qué? Pues porque un npj 
+ no tiene que aparecer siempre en la pantalla, simplemente tiene que estar en el mapa
+ o en un sitio fijo o en un rango pero permanece en el mapa y no en la pantalla. */
+
+/* . Es similar a la clase Personaje pero no es igual, sobretodo por la cuestión anterior. */
+
+/* . Hay que modificar la clase IMAGEN para poder tener varias funcionalidades más: */
+/* . -> Incluir un npj en el mapa y poner que esa/s casilla/s (por si se mueve el personaje)
+        son colisionables e interactuables. */
+/* . -> Además tendremos que poder acceder a que personaje se está colisionando y que interacciones
+        se pueden realizar con él. */
+
+class NPJ {
+ public:
+  NPJ(); // para vectores;
+  NPJ(Uint32 i, Uint32 x, Uint32 y, const char* sprite, Uint32 f = 4, Uint32 c = 4);
+ private:
+
+};
+
+
+
+
 #endif
